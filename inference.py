ebd4f0ea17118e7aa68d8be84b17c3f8db4216b1
th <hirooms2@hanyang.ac.kr>
2025-05-22 12:28:54 +0900

    ADD both pooling

--- inference.py
+++ inference.py
@@ -69,10 +69,10 @@ def inference(args):
     documents = [doc[:prompter.max_char_len * 10] for doc in documents]
     print(len(documents))
     
-    if isinstance(next(iter(db.values())), list):
-        full_passages = []
-        for key in db.keys():
-            full_passages = [", ".join(db[key]) for key in db.keys()]
+    # if isinstance(next(iter(db.values())), list):
+    #     full_passages = []
+    #     for key in db.keys():
+    #         full_passages = [", ".join(db[key]) for key in db.keys()]
     
     all_names = [extract_title_with_year(v) for v in db.values()]
     name2id = {all_names[index]: index for index in range(len(all_names))}
@@ -80,10 +80,9 @@ def inference(args):
     id2name = {v:k for k,v in name2id.items()}
 
     rec_lists = [[name2id[i]] for i in labels]
-    # rec_lists = []
 
     # Loads the model for both capabilities; If you only need embedding pass `mode="embedding"` to save memory (no lm head)
-    model = GritLM("GritLM/GritLM-7B", mode='embedding', torch_dtype="auto", num_items=len(all_names) if args.linear else 0, device="cpu")
+    model = GritLM("GritLM/GritLM-7B", mode='embedding', torch_dtype="auto", num_items=len(all_names) if args.linear else 0)
     # model = GritLM("GritLM/GritLM-7B", torch_dtype="auto")
     
     if args.target_model_path != '':
@@ -99,17 +98,15 @@ def inference(args):
     passages_for_instruction = []
     if args.instruction_aug:
         for target_p in tqdm(db.values()):
-            temp = [passage for passage in db.values() if passage != target_p and extract_title_with_year(passage)==extract_title_with_year(target_p)]
+            temp = [passage for passage in db.values() if passage != target_p 
+                    and extract_title_with_year(passage)==extract_title_with_year(target_p)
+                    and 'N/A' not in passage]
             instruction_passage = ''
             for p in temp:
                 p = p+' '
                 instruction_passage += p
             passages_for_instruction.append(instruction_passage)
 
-    # 마스크 생성 (N/A가 포함된 passage에 마스킹)
-    # masks = torch.tensor([0 if "N/A" in doc else 1 for doc in documents])  # shape: len(documents)
-    # print("mask shape: ", masks.shape)    
-    # mask_tensor = masks.view(1, len(name2id), len(db.values())//len(name2id)).expand(16,1420,5) # (B, num_items, feature)
     
     d_rep= []
     for i, sample in enumerate(tqdm(documents, desc="Encoding documents")):
@@ -121,17 +118,16 @@ def inference(args):
         else:
             instruction = doc_instr
         d_rep.append(model.encode(batch_documents, instruction=gritlm_instruction(instruction))) # self-attention 적용하려면 encode batch size 아이템에 대한 passage 개수로 설정해야함 
-    
     d_rep=np.stack(d_rep, axis=0)
     print('document shape:',torch.from_numpy(d_rep).shape)
 
-    
+
     # 마스크 생성 (N/A가 포함된 passage에 마스킹)
     masks = torch.tensor([0 if "N/A" in doc else 1 for doc in documents])  # shape: len(documents)
     print("mask shape: ", masks.shape)
     
     
-    rank = []
+    rank, rank2 = []
     conf = []
     passages = []
 
@@ -150,35 +146,82 @@ def inference(args):
             cos_sim = torch.where(torch.isnan(cos_sim), torch.full_like(cos_sim,0), cos_sim)
             cos_sim = cos_sim.masked_fill(mask_tensor==0, float('-inf')) # 마스킹 적용
             cos_sim = torch.softmax(cos_sim/0.02, dim=-1)
+            
+            if args.pooling in ['max', 'both']:
+                max_sim = cos_sim.view(len(q_rep), len(name2id), len(db.values())//len(name2id))
+                max_pooled_sim = max_sim.max(dim=-1).values # (B, num_items)
+                topk_sim_values, topk_max_indices = torch.topk(max_pooled_sim, k=50, dim=-1)
+                topk_sim_indices = topk_max_indices * 5
+                
+                # rank += topk_max_indices.tolist()
+                
+            if args.pooling in ['mean', 'both']:
+                cos_sim = cos_sim.view(len(q_rep), len(name2id), len(db.values())//len(name2id)) # (B, num_items, feature)
+                mask_tensor = masks.view(1, len(name2id), len(db.values())//len(name2id)).expand_as(cos_sim) # (B, num_items, feature)
+                sum_sim = cos_sim.sum(dim=-1)
+                passage_count = mask_tensor.sum(dim=-1)
+                mean_pooled_sim = sum_sim / passage_count
+                topk_sim_values, topk_mean_indices = torch.topk(mean_pooled_sim, k=50, dim=-1) # 아이템에 대한 점수 이기 때문에 다시 passage 인덱스로 바꿔줘서 DB 내에 있는 값과 매칭되도록 함
+                
+                if args.pooling=='mean':
+                    topk_sim_indices = topk_mean_indices * 5
+                elif args.pooling=='both':
+                    topk_mean_indices = topk_mean_indices * 5
+                
+                # if args.pooling == 'mean':
+                #     rank += topk_mean_indices.tolist()
+                # elif args.pooling == 'both':
+                #     rank2 += topk_mean_indices.tolist()
+        
         else:
             cos_sim = model.item_proj(torch.from_numpy(q_rep))
             cos_sim = torch.softmax(cos_sim/args.tau, dim=-1)
+            topk_sim_values, topk_sim_indices = torch.topk(cos_sim,k=50,dim=-1)
+            
         # print("cos_sim shape:", cos_sim.shape)
         # print("cos_sim:", cos_sim)
 
-        if args.pooling == 'max':
-            cos_sim = cos_sim.view(len(q_rep), len(name2id), len(db.values())//len(name2id))
-            pooled_sim = cos_sim.max(dim=-1).values # (B, num_items)
-            topk_sim_values, topk_sim_indices = torch.topk(pooled_sim, k=50, dim=-1)
-            topk_sim_indices = topk_sim_indices * 5
+        # if args.pooling == 'max':
+        #     cos_sim = cos_sim.view(len(q_rep), len(name2id), len(db.values())//len(name2id))
+        #     pooled_sim = cos_sim.max(dim=-1).values # (B, num_items)
+        #     topk_sim_values, topk_sim_indices = torch.topk(pooled_sim, k=50, dim=-1)
+        #     topk_sim_indices = topk_sim_indices * 5
         
-        elif args.pooling == 'mean':
-            cos_sim = cos_sim.view(len(q_rep), len(name2id), len(db.values())//len(name2id)) # (B, num_items, feature)
-            mask_tensor = masks.view(1, len(name2id), len(db.values())//len(name2id)).expand_as(cos_sim) # (B, num_items, feature)
+        # elif args.pooling == 'mean':
+        #     cos_sim = cos_sim.view(len(q_rep), len(name2id), len(db.values())//len(name2id)) # (B, num_items, feature)
+        #     mask_tensor = masks.view(1, len(name2id), len(db.values())//len(name2id)).expand_as(cos_sim) # (B, num_items, feature)
             
-            sum_sim = cos_sim.sum(dim=-1)
-            passage_count = mask_tensor.sum(dim=-1)
-            pooled_sim = sum_sim / passage_count
+        #     sum_sim = cos_sim.sum(dim=-1)
+        #     passage_count = mask_tensor.sum(dim=-1)
+        #     pooled_sim = sum_sim / passage_count
             
-            topk_sim_values, topk_sim_indices = torch.topk(pooled_sim, k=50, dim=-1) # 아이템에 대한 점수 이기 때문에 다시 passage 인덱스로 바꿔줘서 DB 내에 있는 값과 매칭되도록 함
-            topk_sim_indices = topk_sim_indices * 5
+        #     topk_sim_values, topk_sim_indices = torch.topk(pooled_sim, k=50, dim=-1) # 아이템에 대한 점수 이기 때문에 다시 passage 인덱스로 바꿔줘서 DB 내에 있는 값과 매칭되도록 함
+        #     topk_sim_indices = topk_sim_indices * 5
             
         
-        else:
-            topk_sim_values, topk_sim_indices = torch.topk(cos_sim,k=50,dim=-1)
+        # elif args.pooling == 'both':
+        #     cos_sim = cos_sim.view(len(q_rep), len(name2id), len(db.values())//len(name2id))
+            
+        #     # max 계산
+        #     max_pooled_sim = cos_sim.max(dim=-1).values # (B, num_items)
+        #     topk_sim_values, topk_sim_indices = torch.topk(max_pooled_sim, k=50, dim=-1)
+        #     topk_sim_indices = topk_sim_indices * 5
+            
+        #     # mean 계산
+        #     mask_tensor = masks.view(1, len(name2id), len(db.values())//len(name2id)).expand_as(cos_sim) # (B, num_items, feature)
+            
+        #     sum_sim = cos_sim.sum(dim=-1)
+        #     passage_count = mask_tensor.sum(dim=-1)
+        #     mean_pooled_sim = sum_sim / passage_count
+            
+        #     topk_mean_values, topk_mean_indices = torch.topk(mean_pooled_sim, k=50, dim=-1) # 아이템에 대한 점수 이기 때문에 다시 passage 인덱스로 바꿔줘서 DB 내에 있는 값과 매칭되도록 함
+        #     topk_mean_indices = topk_mean_indices * 5
+        
+        # else:
+        #     topk_sim_values, topk_sim_indices = torch.topk(cos_sim,k=50,dim=-1)
         
         rank_slice = topk_sim_indices.tolist()
-        if 'passage' in db_path:
+        if 'passage' in db_path: # mean, max, pooling X 버전 돌아갈때 사용됨 
             for i in range(len(topk_sim_indices)):
                 temp_passages = []
                 for j, p in enumerate(rank_slice[i]):
@@ -187,17 +230,36 @@ def inference(args):
                     rank_slice[i][j] = passage2idx
                 passages.append(temp_passages)
     
+        if args.pooling == 'both': # mean pooling 결과 추가로 넣어주기
+            rank2_slice = topk_mean_indices.tolist()
+            for i in range(len(topk_mean_indices)):
+                temp_passages = []
+                for j, p in enumerate(rank2_slice[i]):
+                    temp_passages.append(documents[p])
+                    passage2idx = name2id[extract_title_with_year(documents[p])]
+                    rank2_slice[i][j] = passage2idx
+                passages.append(temp_passages)
+            rank2 += rank2_slice
+        
         
         rank += rank_slice # extend
         conf_slice = topk_sim_values.tolist()
         conf += conf_slice
         print('rank length:',  len(rank))
         print('passages length:',len(passages))
-
+        
+    
     print('model path:', model_path)
     print('length rank:',len(rank))
     recall_score(rec_lists, rank, ks=[1,3,5,10,20,50])
+    
+    if args.pooling=='both':
+        print('model path:', model_path)
+        print('length rank:',len(rank))
+        print('mean pooling 결과')
+        recall_score(rec_lists, rank2, ks=[1,3,5,10,20,50])
 
+    
     if args.store_results:
         for i in tqdm(range(len(rank))):
 
