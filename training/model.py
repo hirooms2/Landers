e433b8f085a74426a516c8f73c05a2ef1c08b5b7
th <hirooms2@hanyang.ac.kr>
2025-05-29 11:47:18 +0900

    0529

--- training/model.py
+++ training/model.py
@@ -261,22 +261,22 @@ class GritLMTrainModel(GritLM):
                 elif self.pooling_emb == 'attention':
                     pos_key_mask = pos_mask.unsqueeze(1)   # [B, 1, 5] 
                     pos_query_mask = pos_mask.unsqueeze(2) # [B, 5, 1] 
-                    pos_key = self.linear_key(pos_reps)
-                    pos_query = self.linear_query(pos_reps)
+                    # pos_key = self.linear_key(pos_reps)
+                    # pos_query = self.linear_query(pos_reps)
                 
-                    pos_attention = torch.matmul(pos_query, pos_key.transpose(-2, -1))  # [B, 5, 5]
+                    pos_attention = torch.matmul(pos_reps, pos_reps.transpose(-2, -1))  # [B, 5, 5]
                     pos_attention = pos_attention.masked_fill(~pos_key_mask, float('-inf'))
                     pos_attention = torch.softmax(pos_attention, dim=-1)
                     pos_reps = torch.matmul(pos_attention, pos_reps) * pos_query_mask  # [B, 5, d]
 
-                    neg_key_mask = neg_mask.unsqueeze(1)   # [B, 1, 5]
-                    neg_query_mask = neg_mask.unsqueeze(2) # [B, 5, 1] 
+                    # neg_key_mask = neg_mask.unsqueeze(1)   # [B, 1, 5]
+                    # neg_query_mask = neg_mask.unsqueeze(2) # [B, 5, 1] 
                     
-                    neg_attention = torch.matmul(neg_reps, neg_reps.transpose(-2, -1))  # [B, 5, 5]
-                    neg_attention = neg_attention.masked_fill(~neg_key_mask, float('-inf'))
-                    neg_attention = torch.softmax(neg_attention, dim=-1)
+                    # neg_attention = torch.matmul(neg_reps, neg_reps.transpose(-2, -1))  # [B, 5, 5]
+                    # neg_attention = neg_attention.masked_fill(~neg_key_mask, float('-inf'))
+                    # neg_attention = torch.softmax(neg_attention, dim=-1)
 
-                    neg_reps = torch.matmul(neg_attention, neg_reps) * neg_query_mask  # [B, 5, d]
+                    # neg_reps = torch.matmul(neg_attention, neg_reps) * neg_query_mask  # [B, 5, d]
 
                 # Concat
                 p_reps = pos_reps  # [B, 4096]
